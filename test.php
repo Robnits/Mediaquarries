3f8776884ee69013b6533cb7d2ede2c8cdb04c25
Robnits <98857700+Robnits@users.noreply.github.com>
2023-11-10 04:59:37 +0100

    Add files via upload

---
+++ test.php
@@ -0,0 +1,15 @@
+<html>
+
+<body>
+    <?php
+        echo $_GET["Email"] . "\r\n";
+        echo $_GET["Anrede"];
+        echo $_GET["Birthdate"];
+        echo $_GET["firstname"];
+        echo $_GET["Lastname"];
+        echo $_GET["Telephone"];
+        echo $_GET["Street"];
+        echo $_GET["ort"];
+    ?>
+</body>
+</html>
